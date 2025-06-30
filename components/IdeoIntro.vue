86d1378ed293f3ecf43d510d85d21c42d577cb53
Pam Weng <quafff@gmail.com>
2025-07-01 00:50:28 +0800

    feat(programming-projects): add service intro

---
+++ components/IdeoIntro.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div
+      class="brook-call-to-action bg_image--9 mt--100"
+      :style="{
+        backgroundImage: `url(${useAssetUrl(props.data.bgImage)})`,
+      }"
+    >
+      <div class="bg_color--14 text_area">
+        <div class="call-content text-center text-sm-start">
+          <h2 class="heading heading-h2 wow move-up">
+            {{ props.data.title }}
+          </h2>
+          <h4 class="heading heading-h4 wow move-up">
+            {{ props.data.subTitle }}
+          </h4>
+          <p v-for="p in props.data.content">
+            {{ p }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  data: {
+    type: Object,
+    default: () => ({
+      bgImage: "",
+      title: "",
+      subTitle: "",
+      content: [],
+    }),
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.brook-call-to-action {
+  height: 150vh;
+}
+.text_area {
+  // background-color: $color-10;
+  opacity: 0.95;
+  // height: 100vh;
+  .call-content {
+    padding: 120px 150px;
+  }
+  .heading {
+    &-h2 {
+      margin-bottom: 30px;
+    }
+    &-h4 {
+      margin-bottom: 50px;
+    }
+  }
+  p {
+    font-weight: 600;
+    font-size: 20px;
+    + p {
+      margin-top: 20px;
+    }
+  }
+}
+</style>
--- pages/programming-projects.vue
+++ pages/programming-projects.vue
@@ -1,67 +1,89 @@
 <template>
   <div>
-    <div class="breadcrumb-area bg_color--15 breadcrumb-title-bar pt--50 pt_md--40 pt_sm--30">
+    <div
+      class="breadcrumb-area bg_color--15 breadcrumb-title-bar pt--50 pt_md--40 pt_sm--30"
+    >
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
             <div class="breadcrumb-inner text-center">
-              <h6 class="heading heading-h6 theme-color font-blod">Our programming projects</h6>
+              <h6 class="heading heading-h6 theme-color font-blod">
+                Our programming projects
+              </h6>
             </div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="bk-blog-grid-area pt--50 pt_md--40 pt_sm--30 pb--100 pb-md--80 pb-sm--60 bg_color--15">
+    <div
+      class="bk-blog-grid-area pt--50 pt_md--40 pt_sm--30 pb--100 pb-md--80 pb-sm--60 bg_color--15"
+    >
       <div class="container">
         <div class="row">
           <div class="controls messonry-button text-center w-100">
             <button data-filter="all">
-              <span class="filter-text">All</span> <span class="filter-counter">6</span>
+              <span class="filter-text">All</span>
+              <span class="filter-counter">6</span>
             </button>
             <button data-filter=".GDG">
-              <span class="filter-text">GDG</span> <span class="filter-counter">3</span>
+              <span class="filter-text">GDG</span>
+              <span class="filter-counter">3</span>
             </button>
             <button data-filter=".Hackathons">
-              <span class="filter-text">各種松們</span> <span class="filter-counter">2</span>
+              <span class="filter-text">各種松們</span>
+              <span class="filter-counter">2</span>
             </button>
             <button data-filter=".Public-Private">
-              <span class="filter-text">公私協力</span> <span class="filter-counter">2</span>
+              <span class="filter-text">公私協力</span>
+              <span class="filter-counter">2</span>
             </button>
             <button data-filter=".Traditional-Industries">
-              <span class="filter-text">傳統產業</span> <span class="filter-counter">2</span>
+              <span class="filter-text">傳統產業</span>
+              <span class="filter-counter">2</span>
             </button>
             <button data-filter=".Other-Industries">
-              <span class="filter-text">其他產業</span> <span class="filter-counter">2</span>
+              <span class="filter-text">其他產業</span>
+              <span class="filter-counter">2</span>
             </button>
           </div>
         </div>
         <div class="row mtn--50">
-          <div class="col-lg-4 col-sm-6 move-up wow mt--100" v-for="blog in data.blogs.slice(26, 32)" :key="blog.id">
-            <BlogPostFive :blog="blog" addClass="blog-standard blog-grid--modern" readMoreButton="true" />
+          <div
+            class="col-lg-4 col-sm-6 move-up wow mt--100"
+            v-for="blog in data.blogs.slice(26, 32)"
+            :key="blog.id"
+          >
+            <BlogPostFive
+              :blog="blog"
+              addClass="blog-standard blog-grid--modern"
+              readMoreButton="true"
+            />
           </div>
         </div>
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="brook-pagination-wrapper text-center pt--80">
-              <ul class="brook-pagination">
-                <li class="page-item">
-                  <button>Prev</button>
-                </li>
-                <li class="page-item active">
-                  <button>1</button>
-                </li>
-                <li class="page-item">
-                  <button>2</button>
-                </li>
-                <li class="page-item">
-                  <button>3</button>
-                </li>
-                <li class="page-item">
-                  <button>Next</button>
-                </li>
-              </ul>
-            </div>
+      </div>
+      <IdeoIntro :data="programmingProjectsData" />
+
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="brook-pagination-wrapper text-center pt--80">
+            <ul class="brook-pagination">
+              <li class="page-item">
+                <button>Prev</button>
+              </li>
+              <li class="page-item active">
+                <button>1</button>
+              </li>
+              <li class="page-item">
+                <button>2</button>
+              </li>
+              <li class="page-item">
+                <button>3</button>
+              </li>
+              <li class="page-item">
+                <button>Next</button>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -71,6 +93,7 @@
 
 <script setup>
 import data from "@data/blog.json";
+import { programmingProjectsData } from "@data/serviceIntro.json";
 
 const navOpen = ref(false);
 const searchOpen = ref(false);
