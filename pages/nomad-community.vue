cf2db1a80d3c222f1f3ec9b4f96824831e88f192
Pam Weng <quafff@gmail.com>
2025-07-01 15:18:12 +0800

    feat(nomads-community): modify the color style

--- pages/nomad-community.vue
+++ pages/nomad-community.vue
@@ -67,10 +67,10 @@ const searchOpen = ref(false);
 
 // 套用/移除樣式
 onMounted(() => {
-  document.body.classList.add("template-color-10", "template-font-1");
+  document.body.classList.add("template-color-7", "template-font-1");
 });
 onUnmounted(() => {
-  document.body.classList.remove("template-color-10", "template-font-1");
+  document.body.classList.remove("template-color-7", "template-font-1");
 });
 
 definePageMeta({
